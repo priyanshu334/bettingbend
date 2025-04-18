738e75665e04eb61b2cc88429b5c61429c2a1ade
priyanshu334 <99priyanshu33@gmail.com>
2025-04-18 23:43:17 +0530

    ok

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const mongoURI = process.env.MONGODB_URI;
+    const mongoURI = process.env.MONGO_URI;
 
     if (!mongoURI) {
       throw new Error("MONGODB_URI is not defined in the environment variables");
